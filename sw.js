fa3a10b6283b6aca55383e4ffcd77785f4e983c0
MoriMTOT <Morteza.fadakaran@gmail.com>
2026-07-08 14:58:05 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,16 @@
-const CACHE_NAME='talkboard-wordclean-v12';
-const CORE_FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_FILES)));
+// Disabled service worker to stop old cache.
+self.addEventListener("install", event => {
+self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.map(key => caches.delete(key)))
+).then(() => self.clients.claim())
+);
 });
 
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  event.respondWith(
-    fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
-      return res;
-    }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html')))
-  );
+self.addEventListener("fetch", event => {
+event.respondWith(fetch(event.request));
 });
